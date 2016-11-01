cbf0a05656937487240a75a224e37ced76627c7c
Arun Michael Dsouza <amdsouza92@gmail.com>
2016-11-02 02:19:39 +0530

    let+const block scoping, default params, object shorthand properties, spread operator implementation

--- js/build/bundle.js
+++ js/build/bundle.js
@@ -44,14 +44,20 @@
 /* 0 */
 /***/ function(module, exports) {
 
-	"use strict";
+	'use strict';
+
+	var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
+	var _console;
+
+	function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 
 	/* 
 		Arrow Functions
 	*/
 	var square = function square(num) {
 		return num * num;
-	};
+	}; // Arrow shorthand method
 	console.log(square(10));
 
 	var calculate = function calculate(a, b) {
@@ -78,10 +84,72 @@
 		getDoubled: function getDoubled() {
 			return this.arr.map(function (i) {
 				return i * 2;
-			});
+			}); // Access parent scope directly using 'this'
 		}
 	};
 	console.log(obj.getDoubled());
+
+	/*
+		const and let + block scoping 
+	*/
+	var int = 10;
+	var char = 'Text1';
+	{
+		var _char = 'Text2'; // 'let' and 'const' enables block scoping
+	}
+	//int = 12; // Not allowed, int is read-only
+	console.log(char);
+	{
+		var b = 'b';
+	}
+	//console.log(b); // undefined
+
+
+	/* 
+		Default parameters
+	*/
+	var fn = function fn(greet) {
+		var name = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 'world';
+		return greet + ' ' + name;
+	};
+	console.log(fn('Hello'));
+	console.log(fn('Hello', 'guys'));
+
+	/*
+		Object shorthand properties
+	*/
+	var firstName = 'Arun',
+	    lastName = 'Dsouza',
+	    func = function func() {
+		return 'Func!';
+	};
+	var user = { firstName: firstName, lastName: lastName, func: func }; // Sets key name as that of variable
+	console.log(user);
+	console.log(user.func());
+
+	/* 
+		Spread operator
+	*/
+	var list = [5, 6, 7, 8, 9];
+	(_console = console).log.apply(_console, list); // Spreads out to individual items
+
+	var add = function add(a, b, c) {
+		return a + b + c;
+	};
+	console.log(add.apply(undefined, [1, 2, 3]));
+
+	var key = 'info',
+	    info = { id: 12, num: 1244 };
+	var userInfo = {
+		name: 'test',
+		email: 'a@a.com'
+	};
+	var detail = _extends({}, userInfo, _defineProperty({}, key, info)); // Spread out objects to add new key/value pairs
+	console.log(detail);
+
+	/* 
+		String templates
+	*/
 
 /***/ }
 /******/ ]);
--- js/src/script.js
+++ js/src/script.js
@@ -1,7 +1,7 @@
 /* 
 	Arrow Functions
 */
-const square = num => num * num;
+const square = num => num * num; // Arrow shorthand method
 console.log(square(10));
 
 const calculate = (a, b) => {
@@ -26,8 +26,72 @@ console.log(reducedValue);
 const obj = {
 	arr: [1,2,3,4,5],
 	getDoubled() {
-		return this.arr.map(i => i * 2);
+		return this.arr.map(i => i * 2); // Access parent scope directly using 'this'
 	},
 }
 console.log(obj.getDoubled());
+
+
+
+/*
+	const and let + block scoping 
+*/
+const int = 10;
+let char = 'Text1';
+{
+	let char = 'Text2'; // 'let' and 'const' enables block scoping
+}
+//int = 12; // Not allowed, int is read-only
+console.log(char);
+{
+	const b = 'b';
+}
+//console.log(b); // undefined
+
+
+
+
+/* 
+	Default parameters
+*/
+const fn = (greet, name='world') => greet + ' ' + name;
+console.log(fn('Hello')); 
+console.log(fn('Hello', 'guys')); 
+
+
+
+/*
+	Object shorthand properties
+*/
+const firstName = 'Arun', lastName = 'Dsouza', func = () => 'Func!';
+const user = { firstName, lastName, func }; // Sets key name as that of variable
+console.log(user);
+console.log(user.func());
+
+
+
+/* 
+	Spread operator
+*/
+const list = [5,6,7,8,9];
+console.log(...list); // Spreads out to individual items
+
+const add = (a, b, c) => a + b + c;
+console.log(add(...[1,2,3]));
+
+
+const key = 'info', info = { id: 12, num: 1244 };
+const userInfo = {
+	name: 'test',
+	email: 'a@a.com'
+};
+const detail = { ...userInfo, [key]: info }; // Spread out objects to add new key/value pairs
+console.log(detail);
+
+
+
+/* 
+	String templates
+*/
+
 
